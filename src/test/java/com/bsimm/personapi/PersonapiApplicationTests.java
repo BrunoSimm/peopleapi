2f30107cc1a73b64e80f6e160c80182aaa82ee7e
brunosimm <bruno.simm@edu.pucrs.br>
2021-08-11 21:51:08 -0300

    Inital commit

---
+++ src/test/java/com/bsimm/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsimm.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
